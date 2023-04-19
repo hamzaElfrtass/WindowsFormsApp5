5a263ba3d4cae46a3179fee1118e03f3217ef422
hamzaelfrtass <hamzaelfrtass5@gmail.com>
2023-04-19 14:45:42 +0000

    Ajoutez des fichiers projet.

---
+++ Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace WindowsFormsApp5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.textBoxID = new System.Windows.Forms.TextBox();
+            this.btnSearch = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(147, 152);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(406, 197);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // textBoxID
+            // 
+            this.textBoxID.Location = new System.Drawing.Point(147, 116);
+            this.textBoxID.Name = "textBoxID";
+            this.textBoxID.Size = new System.Drawing.Size(282, 20);
+            this.textBoxID.TabIndex = 1;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(435, 113);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(106, 23);
+            this.btnSearch.TabIndex = 2;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(910, 450);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.textBoxID);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox textBoxID;
+        private System.Windows.Forms.Button btnSearch;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+using System.Net.Http;
+using System.Net.Http.Formatting;
+
+namespace WindowsFormsApp5
+{
+    public partial class Form1 : Form
+    {
+        localhost.WebService1 proxy= new localhost.WebService1();
+        HttpClient client = new HttpClient();   
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void WebServiceSettings()
+        {
+            client.BaseAddress = new Uri("https://localhost:44332/WebService1.asmx/");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //string jsoncountries = proxy.COUNTRIES();
+            //DataTable dtcountries=JsonConvert.DeserializeObject<DataTable>(jsoncountries);
+            //dataGridView1.DataSource= dtcountries;
+            WebServiceSettings();
+        }
+        private DataTable stringsplit(string userjson)
+        {
+            string[] json=userjson.Split('>');
+            string[] finaljson = json[2].Split('<');
+            DataTable dt = JsonConvert.DeserializeObject<DataTable>(finaljson[0]);
+            return dt;
+        }
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            HttpResponseMessage message = client.GetAsync("datatableforusers?id=" + textBoxID.Text + "").Result;
+            string userjson=message.Content.ReadAsStringAsync().Result;
+            //MessageBox.Show(userjson);
+            dataGridView1.DataSource=stringsplit(userjson);
+        }
+    }
+}
